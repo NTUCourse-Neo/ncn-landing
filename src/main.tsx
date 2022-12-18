93f21de0aeac2e1fd75dd332a2901feecba2ab1f
swh00tw <a6140000@gmail.com>
2022-12-18 13:54:39 +0800

    change to abs import

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "@/App";
 import { ChakraProvider } from "@chakra-ui/react";
-import HeaderBar from "./components/HeaderBar";
-import theme from "./styles/theme";
-import Footer from "./components/Footer";
+import HeaderBar from "@/components/HeaderBar";
+import theme from "@/styles/theme";
+import Footer from "@/components/Footer";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

dde197ed326c2bf19a9fac70695c19244dd7fe6c
jc-hiroto <pohaocha@andrew.cmu.edu>
2023-02-23 12:15:19 -0800

    Fix mobile page layout.

--- src/containers/Mobile.tsx
+++ src/containers/Mobile.tsx
@@ -4,42 +4,37 @@ import { useTranslation } from "react-i18next";
 export function Mobile() {
   const { t, i18n } = useTranslation();
   return (
-    <Box
+    <Flex
       maxW="screen-md"
-      minH="85vh"
+      minH="90vh"
       mx="auto"
       overflow="visible"
       px="4"
-      pt="64px"
       bg="black"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
     >
-      <Flex
-        flexDirection="column"
-        w="100%"
-        justifyContent="center"
-        alignItems="center"
+      <Image
+        src={`/img/mobile.svg`}
+        alt="ncnLogo"
+        w="60%"
+        my="16"
+        draggable="false"
+        pointerEvents={"none"}
+      />
+      <Text fontSize="3xl" fontWeight="800" color={"gray.400"} align="center">
+        {t("mobile.title")}
+      </Text>
+      <Text
+        fontSize="sm"
+        fontWeight="400"
+        color={"gray.400"}
+        my="4"
+        align="center"
       >
-        <Image
-          src={`/img/mobile.svg`}
-          alt="ncnLogo"
-          w="60%"
-          my="16"
-          draggable="false"
-          pointerEvents={"none"}
-        />
-        <Text fontSize="3xl" fontWeight="800" color={"gray.400"} align="center">
-          {t("mobile.title")}
-        </Text>
-        <Text
-          fontSize="md"
-          fontWeight="400"
-          color={"gray.400"}
-          my="4"
-          align="center"
-        >
-          {t("mobile.content")}
-        </Text>
-      </Flex>
-    </Box>
+        {t("mobile.content")}
+      </Text>
+    </Flex>
   );
 }

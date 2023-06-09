d074df95dc3e904bf036ea6016c2c7ae57c3914d
swh00tw <a6140000@gmail.com>
2023-06-09 20:31:40 +0800

    hide add button

--- src/containers/FeatureSection.tsx
+++ src/containers/FeatureSection.tsx
@@ -333,6 +333,7 @@ export function FeatureSection() {
                         courseInfo={c}
                         selected={false}
                         displayAddToNol
+                        displayButton={false}
                       />
                     ))}
                 </Accordion>
